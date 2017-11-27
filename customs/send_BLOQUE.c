bca2c5a71d548e2ac5aaa7c4157cb9c29d3243a3
Ignacio Escobar <inec_14@hotmail.com>
2017-11-27 20:55:32 -0300

    correcion Bloque!

--- customs/send_BLOQUE.c
+++ customs/send_BLOQUE.c
@@ -1,22 +1,24 @@
-void send_BLOQUE(int socket, uint64_t tamanio_bloque, char* bloque, uint32_t id_bloque){
+void send_BLOQUE(int socket, int tamanio_bloque, char* bloque, int id_bloque){
 
 	HEADER_T header = BLOQUE;
 
-	char* paquete = malloc(sizeof(HEADER_T)+sizeof(uint64_t)+tamanio_bloque+sizeof(uint32_t));
+	char* paquete = malloc(sizeof(HEADER_T)+sizeof(int)+tamanio_bloque+sizeof(int));
 
 	int offset = 0;
 
 	memcpy(paquete+offset,&header,sizeof(HEADER_T));
 	offset += sizeof(HEADER_T);
 
-	memcpy(paquete+offset,&tamanio_bloque,sizeof(uint64_t));
+	memcpy(paquete+offset,&tamanio_bloque,sizeof(int));
 	offset += sizeof(uint64_t);
 
 	memcpy(paquete+offset,bloque,tamanio_bloque);
 	offset += tamanio_bloque;
 
-	memcpy(paquete+offset,&id_bloque,sizeof(uint32_t));
-	offset += sizeof(uint32_t);
+	memcpy(paquete+offset,&id_bloque,sizeof(int));
+	offset += sizeof(int);
+
+	enviar_paquete(paquete,offset);
 
 	free(paquete);
 
